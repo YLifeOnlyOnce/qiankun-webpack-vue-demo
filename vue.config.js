902cb6ae5af85e031a3d070c08a9bbc6d50e5b5e
yaoxiangbo <yaoxiangbo@qianxin.com>
2025-07-29 15:49:12 +0800

    feat: init config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,33 @@
 const { name } = require('./package');
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
+  publicPath:'/microapps/qiankun-webpack-vue/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  filenameHashing: true,
   devServer: {
-    port: 5175,
+    hot: true,
+    disableHostCheck: true,
+    port: process.env.VUE_APP_PORT,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
   },
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
     output: {
       library: `${name}-[name]`,
       libraryTarget: 'umd', // 把微应用打包成 umd 库格式
